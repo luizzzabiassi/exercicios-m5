ab8ad1ddc69225cce1cab1e32e87650025821dc0
Luiza Biassi <luiza.biassi@gmail.com>
2021-08-03 12:48:45 -0300

    fetch install

--- api.js
+++ api.js
@@ -1,6 +1,8 @@
 // Faça fetch dos dados através da API e retorne a ficha desta refeição com: 
 // id, nome, área (país de origem), ingredientes (em uma única string) e as intruções:
 
+const fetch = require('node-fetch')
+
 fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`)
 .then(obj => {
     dados(obj.results)
@@ -8,6 +10,6 @@ fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`)
 
 function dados(arr){
     arr.forEach(element => {
-        console.log(`Nome: ${element.strMeal} /n ID: ${element.idMeal}`)
+        console.log(`Nome: ${element.strMeal} \n ID: ${element.idMeal} \n País: ${element.strArea} \n Ingredientes: ${element} \n Instruções: ${element.strInstructions}`)
     })
 }
